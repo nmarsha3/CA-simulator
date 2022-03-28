e6772871185d4d2d187ff7737176265824824a6d
Nicholas Marshall <nmarsha3@student05.cse.nd.edu>
2022-03-28 12:59:50 -0400

    Created init Board class for Board of FSTs

---
+++ Board.py
@@ -0,0 +1,28 @@
+class Board:
+
+   def __init__(self, neighborhood="von neumann", dimension=1, length=5):
+      # Take in variables
+      self.neighborhood = neighborhood
+      self.dimension = dimension
+      self.length = length
+
+      # Build the board
+      self.board = self.buildBoard(dimension, length)
+
+   def buildBoard(self, dim, length):
+      if dim == 1:
+         #return [FST(params) for i in range(length)]
+         return [0 for i in range(length)]
+      
+      return [self.buildBoard(dim-1, length) for i in range(length)]
+
+   def printBoard(self):
+      print(self.board)
+
+   def setNeighborhood(self, neighborhood):
+      self.neighborhood = neighborhood
+   
+   def setDimension(self, dimension):
+      self.dimension = dimension
+
+   
