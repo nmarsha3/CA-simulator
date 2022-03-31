791ec3caaa693e33872bda2adea3cf148f644c82
nmarsha3 <64170949+nmarsha3@users.noreply.github.com>
2022-03-31 15:47:28 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,11 +1,12 @@
 import sys
+from Board import Board
 
 def main():
    pass
 
    # TODO: Build Board using Board class
    
-   # TODO: Iterate k times
+   # TODO: Iterate k times updating the CA board
    
 
 if __name__ == "__main__":
