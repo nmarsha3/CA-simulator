cc7a9f6d9a0058fc18186f759ae30160d3b9c665
Nicholas Marshall <nmarsha3@student05.cse.nd.edu>
2022-03-31 13:39:04 -0400

    Created a finished json representation of a CA

--- Board.py
+++ Board.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import json
 
 Cell = namedtuple("Cell", "state next_state output")
 
@@ -13,12 +14,20 @@ class Board:
       # Build the board
       self.board = self.buildBoard(dimension, length)
 
-   def buildBoard(self, dim, length):
+   def buildEmptyBoard(self, dim, length):
       if dim == 1:
          return [Cell(0, 0, 0) for i in range(length)]
       
       return [self.buildBoard(dim-1, length) for i in range(length)]
 
+   def buildJsonBoard(self, json_file):
+
+      f = open(json_file)
+      data = json.load(f)
+
+      for i in data
+
+   
    def printBoard(self):
       print(self.board)
 
