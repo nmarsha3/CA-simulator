0560872da5746f9cfaaaee3a83c58247506b2292
Jacob Mazur <56324444+jmazur2@users.noreply.github.com>
2022-04-08 12:10:54 -0400

    Update Board.py
    fixes

--- Board.py
+++ Board.py
@@ -6,7 +6,7 @@ Cell = namedtuple("Cell", "state next_state output")
 
 class Board:
 
-   def __init__(self, neighborhood="von neumann", dimension=1, length=5):
+   def __init__(self, neighborhood="von neumann", dimension=1, length=5, init_file=None):
       # Take in variables
       self.neighborhood = neighborhood
       self.dimension = dimension
@@ -14,14 +14,26 @@ class Board:
       self.transitions = []
 
       # Build the board
-      self.board = self.buildEmptyBoard(dimension, length)
+            if init_file is not None:
+         self.board = np.asarray(self.buildJsonBoard(init_file))
+      else:
+         self.board = np.asarray(self.buildEmptyBoard(dimension, length))
+
+   def __str__(self):
+
+      if self.dimension == 1:
+         return str(self.board)
+      elif self.dimension == 2:
+         return str(self.board)
+      else:
+         return str(self.board)
 
    def buildEmptyBoard(self, dim, length):
       if dim == 1:
          return [Cell(0, 0, 0) for i in range(length)]
       
-      return np.asarray([self.buildBoard(dim-1, length) for i in range(length)])
-
+      return [self.buildEmptyBoard(dim-1, length) for i in range(length)]
+   
    def buildJsonBoard(self, json_file):
 
       # Open file
@@ -41,7 +53,7 @@ class Board:
          return [recursive_zip(i,j) for i,j in zip(a,b)] 
       
       # Build board with initial states and outputs
-      self.board = np.asarray(recursive_zip(init_states, init_outputs))
+      return recursive_zip(init_states, init_outputs)
       
    def iterate(self):
 
@@ -64,4 +76,4 @@ class Board:
    def setDimension(self, dimension):
       self.dimension = dimension
 
-   
+# vim: set sts=3 sw=3 ts=6 expandtab ft=python: 
