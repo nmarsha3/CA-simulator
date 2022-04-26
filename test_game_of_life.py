08f41d703f240add1ebec4797d239f1c4c547548
Nicholas Marshall <nmarsha3@student05.cse.nd.edu>
2022-04-25 20:09:07 -0400

    decreased the length of blinker and glider

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -6,10 +6,10 @@ BOARD_FILE = 'data/game_of_life_blinker.json'
 
 board = Board(init_file=BOARD_FILE)
 
-board.iterations(50, 0.3)
+board.iterations(5, 0.3)
 
 BOARD_FILE = 'data/game_of_life_glider.json'
 
 board = Board(init_file=BOARD_FILE)
 
-board.iterations(50, 0.3)
+board.iterations(10, 0.3)
